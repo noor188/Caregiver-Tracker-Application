ce97dc365a62c1dd4449d49a665bb4a817b9f92a
noor188 <noormmlk@gmail.com>
2025-03-01 20:47:54 -0700

    layers

---
+++ src/main/java/com/example/spring_boot_crud_api/SpringBootCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudApiApplication.class, args);
+	}
+
+}
